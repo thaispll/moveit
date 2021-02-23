0c2f869b0f6cb2b30b43646ce0967ef90c0a154c
Thais Leal <thais.pll@hotmail.com>
2021-02-23 14:02:47 -0300

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import { Button } from './components/Button';
+import './styles/global.css';
+
+function App() {
+  return (
+    <div>
+        <Button color="red"> Botão 1  </Button>
+        <Button color="blue"> Botão 2  </Button>
+        <Button color="green"> Botão 3  </Button>
+    </div>
+  );
+}
+
+export default App;
